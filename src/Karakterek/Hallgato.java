9660e45128778abb1355675bd1d3019106749173
Szabó Gergő Barnabás <szabogergobarnabas02@gmail.com>
2024-03-22 21:25:36 +0100

    Karakter elvileg kesz, Hallgato felig Oktato semennyire

--- src/Karakterek/Hallgato.java
+++ src/Karakterek/Hallgato.java
@@ -1,4 +1,72 @@
 package Karakterek;
 
-public class Hallgato {
+import Szoba.Szoba;
+import Targyak.Logarlec;
+import Targyak.Romlandok.Camambert;
+import Targyak.Romlandok.Rongy;
+import Targyak.Tranzisztor;
+
+import java.util.Scanner;
+
+public class Hallgato extends Karakter {
+	void aktival() {
+		System.out.println("Hallgato aktival egy targyat\n");
+		System.out.println("A targyak amiket magadnal viselsz:\n" +
+				"1. Tranzisztor\n" +
+				"2. Rongy\n" +
+				"3. Camambert\n" +
+				"4. Logarlec\n");
+		Scanner scanner = new Scanner(System.in);
+		int choice = scanner.nextInt();
+
+		switch (choice) {
+			case 1:
+				System.out.println("Tranzisztor aktivalva\n");
+				Tranzisztor tranzisztor = new Tranzisztor();
+				tranzisztor.aktival();
+				break;
+			case 2:
+				System.out.println("Rongy aktivalva\n");
+				Rongy rongy = new Rongy();
+				break;
+			case 3:
+				System.out.println("Camambert aktivalva\n");
+				Camambert camambert = new Camambert();
+				camambert.aktival();
+				break;
+			case 4:
+				System.out.println("Logarlec aktivalva\n");
+				Logarlec logarlec = new Logarlec();
+				logarlec.aktival();
+				break;
+			default:
+				System.out.println("Nincs ilyen targy\n");
+				break;
+		}
+		System.out.println("A kivalasztott targy aktivalva\n");
+	}
+
+	@Override
+	public void mozog(Szoba newSzoba) {
+		System.out.println("Hallgato mozog\n");
+		super.mozog(newSzoba);
+		newSzoba.getAktivTargy();
+		System.out.println("A szoba gazos? (I/N)\n");
+		Scanner scanner = new Scanner(System.in);
+		String choice = scanner.nextLine();
+
+		if (choice.equals("I")) {
+			System.out.println("A szoba gazos\n");
+			System.out.println("Hallgatonak van maszkja?\n");
+			String mask = scanner.nextLine();
+			if (mask.equals("I")) {
+				System.out.println("A hallgato felhasznalja a maszkjat\n");
+			} else {
+				System.out.println("A hallgato elejt mindent\n");
+				mindentelejt();
+			}
+		} else {
+			System.out.println("A szoba nem gazos\n");
+		}
+	}
 }
--- src/Karakterek/Karakter.java
+++ src/Karakterek/Karakter.java
@@ -1,4 +1,79 @@
 package Karakterek;
 
+import Szoba.Szoba;
+import Targyak.Targyinventory;
+
+import java.util.Scanner;
+
 public class Karakter {
+	public void felvesz() {
+		System.out.println("Targy felvetel\n" +
+				"Milyen targyat szeretnel felvenni?\n");
+		Szoba szoba = new Szoba();
+		szoba.getBentiTargyak();
+		Targyinventory eszkozok = new Targyinventory();
+		Scanner scanner = new Scanner(System.in);
+		String targy = scanner.nextLine();
+		System.out.println("A kivalasztott targy: " + targy + " felveve\n");
+		if (eszkozok.AddTargy()) {
+			System.out.println("A targy felvetele sikeres\n");
+			szoba.RemoveTargy();
+		} else {
+			System.out.println("A targy felvetele sikertelen\n");
+		}
+
+	}
+
+	public void letesz() {
+		System.out.println("Targy letetel\n" +
+				"Milyen targyat szeretnel letenni?\n");
+		System.out.println("A targyak amiket magadnal viselsz:\n" +
+				"1. Tvsz\n" +
+				"2. Söröspohár\n" +
+				"3. Tranzisztor\n" +
+				"4. Rongy\n" +
+				"5. Camambert\n" +
+				"6. Maszk\n");
+		Scanner scanner = new Scanner(System.in);
+		String targy = scanner.nextLine();
+		System.out.println("A kivalasztott targy leteve\n");
+		Szoba szoba = new Szoba();
+		szoba.setBentiTargyak();
+	}
+
+	public void mozog(Szoba newSzoba) {
+		Szoba old = new Szoba();
+		if (newSzoba.beenged()) {
+			System.out.println("A szobaban van eleg hely\n");
+			old.kilep(this);
+		} else {
+			System.out.println("A szoba tele nincs hely\n");
+		}
+	}
+
+	public Szoba getSzoba() {
+		return null;
+	}
+
+	public void mindentelejt() {
+		System.out.println("Minden targy eldobva\n");
+		Szoba szoba = new Szoba();
+		Targyinventory eszkozok = new Targyinventory();
+		szoba.setBentiTargyak(eszkozok);
+		eszkozok.targyak.clear();
+	}
+
+	public Karakter getID() {
+		return null;
+	}
+
+	public Targyinventory getEszkozkeszlet() {
+		return null;
+	}
+
+	public void setEszkozkeszlet(Targyinventory eszkozok) {
+		System.out.println("Eszkozkeszlet beallitasa\n");
+	}
+
+
 }
