07c78383bff712e801cf04691522015efeebd3fa
Iváncsics Ágoston Márton <agoston.ivancsics@edu.bme.hu>
2024-04-29 04:55:45 +0200

    teszt 18 + Sorospohár letesz egy tárgyat

--- src/logarlec/Sorospohar.java
+++ src/logarlec/Sorospohar.java
@@ -13,9 +13,18 @@ public class Sorospohar extends Romlandok{
     public boolean hasznal(Karakter k) {
         System.out.println("So_hasznalva");
         // A játékos eszközkészletéből egy véletlenszerűen kiválasztott tárgy eltávolítása
-        int targyakSzama = k.getEszkozkeszlet().getTargyak().size();
-        int randomIndex = (int) (Proto.getRandVal() * (targyakSzama + 1));
-        k.getEszkozkeszlet().getTargyak().remove(randomIndex);
+        double rand = Proto.getRandVal();
+		if(rand >= 0.5 || !k.getEszkozkeszlet().getTargyak().isEmpty()){
+		    int randint = (int) (rand * 10);
+		    if(randint > k.getEszkozkeszlet().getTargyak().size()){
+			    randint = k.getEszkozkeszlet().getTargyak().size()-1;
+		    }
+            k.getEszkozkeszlet().getTargyak().remove(randint);
+		    System.out.println("Item_leteve");
+        }
+        else {
+            System.out.println("Nincs_targy_a_jatekosnal_nem_dob_el_targyat");
+        }
         // A tárgy használata
         romlik();//_hatralevoido_csokken
         if (getHatralevoIdo() == 0) {
