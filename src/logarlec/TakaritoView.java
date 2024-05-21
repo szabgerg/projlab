f2ba39f56604e9ca0fefe3cbf237383f8b6f2586
Balogh Abigél Emese <dabiabi1@gmail.com>
2024-05-21 21:23:09 +0200

    Takarító kommentek

--- src/logarlec/TakaritoView.java
+++ src/logarlec/TakaritoView.java
@@ -6,11 +6,17 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * A TakaritoView osztály felelős a takarítók megjelenítéséért.
+ */
 public class TakaritoView extends KarakterView{
+    // A takarító képének fájl elérési útja
     private static final String TAKARITO_IMAGE_PATH = "kepek/takarito.png";
     
+    // A takarító képe, amelyet statikusan betöltünk
     private static Image takaritoImage;
 
+    // Statikus inicializáló blokk a kép betöltéséhez
     static{
         try{
             takaritoImage = ImageIO.read(new File(TAKARITO_IMAGE_PATH));
@@ -21,25 +27,62 @@ public class TakaritoView extends KarakterView{
         }
     }
 
+    // A Takarito modell, amelyet ez a nézet megjelenít
     private Takarito model;
+    // A koordináták, ahol a takarító megjelenik
     private Coordinates cd;
 
+    /**
+     * Alapértelmezett konstruktor, amely üres modellt és koordinátákat állít be.
+     */
     public TakaritoView(){
         this.model = null;
         this.cd = new Coordinates(0,0);
     }
 
+    /**
+     * Konstruktor, amely beállítja a modellt és a koordinátákat.
+     *
+     * @param model A Takarito modell
+     * @param cd A koordináták, ahol a takarító megjelenik
+     */
     public TakaritoView(Takarito model, Coordinates cd){
         this.model = model;
         this.cd = cd;
     }
+
+    /**
+     * Getter a koordinátákhoz.
+     *
+     * @return A koordináták, ahol a takarító megjelenik
+     */
     public Coordinates getCd() {return cd;}
 
+    /**
+     * Setter a koordinátákhoz.
+     *
+     * @param cd A koordináták, ahol a takarító megjelenik
+     */
     public void setCd(Coordinates cd) {this.cd = cd;}
 
+    /**
+     * Getter a modellhez.
+     *
+     * @return A Takarito modell
+     */
     protected Takarito getModel(){return model;}
+    /**
+     * Setter a modellhez.
+     *
+     * @param model A Takarito modell
+     */
     protected void setModel(Takarito model){this.model=model;}
 
+    /**
+     * A Graphics objektum segítségével megrajzoljuk a takarítót.
+     *
+     * @param g A Graphics objektum, amellyel rajzolunk
+     */
     @Override
     public void draw(Graphics g) {
         if (takaritoImage != null){
