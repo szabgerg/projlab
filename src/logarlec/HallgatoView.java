0e9704a008dc75964157de7ca396ea5c0645cb3d
Balogh Abigél Emese <dabiabi1@gmail.com>
2024-05-21 12:35:19 +0200

    Oktató, Hallgató kommentek

--- src/logarlec/HallgatoView.java
+++ src/logarlec/HallgatoView.java
@@ -7,11 +7,19 @@ import java.util.Random;
 
 import javax.imageio.ImageIO;
 
+/**
+ * HallgatoView osztály, amely egy hallgató megjelenítéséért felelős.
+ */
 public class HallgatoView extends KarakterView{
+
+    // Hallgató fiú és lány képének fájl elérési útjai
     private static final String HALLGATO_IMAGE_PATH_FIU = "kepek/fiu.png";
     private static final String HALLGATO_IMAGE_PATH_LANY = "kepek/lany.png";
+    
+    // A hallgató képe, amelyet statikusan betöltünk
     private static Image hallgatoImage;
 
+    // Statikus inicializáló blokk a kép betöltéséhez
     static{
         try{
 	        Random rand = new Random();
@@ -28,35 +36,75 @@ public class HallgatoView extends KarakterView{
             System.out.println("Failed to load image: " + HALLGATO_IMAGE_PATH_LANY);
         }
     }
+
+    // A Hallgato modell, amelyet ez a nézet megjelenít
     private Hallgato model;
+
+    // A koordináták, ahol a hallgató megjelenik
     private Coordinates cd;
 
+     /**
+     * Alapértelmezett konstruktor, amely üres modellt és koordinátákat állít be.
+     */
     public HallgatoView() {
         this.model = null;
         this.cd = new Coordinates(0, 0);
     }
 
+    /**
+     * Konstruktor, amely beállítja a modellt és a koordinátákat.
+     *
+     * @param model A Hallgato modell
+     * @param cd A koordináták, ahol a hallgató megjelenik
+     */
     public HallgatoView(Hallgato model, Coordinates cd){
         this.model = model;
         this.cd = cd;
     }
 
+     /**
+     * Getter a koordinátákhoz.
+     *
+     * @return A koordináták, ahol a hallgató megjelenik
+     */
     public Coordinates getCd() {return cd;}
 
+    /**
+     * Setter a koordinátákhoz.
+     *
+     * @param cd A koordináták, ahol a hallgató megjelenik
+     */
     public void setCd(Coordinates cd) {this.cd = cd;}
 
+    /**
+     * Védett getter a modellhez.
+     *
+     * @return A Hallgato modell
+     */
     protected Hallgato getModel(){return model;}
+
+    /**
+     * Védett setter a modellhez.
+     *
+     * @param model A Hallgato modell
+     */
     protected void setModel(Hallgato model){this.model=model;}
 
+    /**
+     * A Graphics objektum segítségével megrajzoljuk a hallgatót.
+     *
+     * @param g A Graphics objektum, amellyel rajzolunk
+     */
     @Override
     public void draw(Graphics g) {
         if (hallgatoImage != null){
+            // Ha a kép sikeresen betöltődött, megrajzoljuk
             g.drawImage(hallgatoImage, cd.getX(), cd.getY(),115 , 300, null);
         }
         else{
-            // Draw a placeholder rectangle if the image failed to load
+            // Ha a kép betöltése nem sikerült, egy helykitöltő téglalapot rajzolunk
             g.setColor(Color.RED);
-            g.fillRect(cd.getX(), cd.getY(), 100, 100); // Adjust the size as needed
+            g.fillRect(cd.getX(), cd.getY(), 100, 100); // Igazítsa a méretet szükség szerint
         }
     }
 }
--- src/logarlec/OktatoView.java
+++ src/logarlec/OktatoView.java
@@ -5,12 +5,24 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**
+ * OktatoView osztály, amely egy oktató megjelenítéséért felelős.
+ */
 public class OktatoView extends KarakterView{
+
+    // Oktató képének fájl elérési útja
     private static final String OKTATO_IMAGE_PATH = "kepek/oktato.png";
+    
+    // Az Oktato modell, amelyet ez a nézet megjelenít
     private Oktato model;
+
+    // A koordináták, ahol az oktató megjelenik
     private Coordinates cd;
+
+    // Az oktató képe, amelyet statikusan betöltünk
     private static Image oktatoImage;
 
+    // Statikus inicializáló blokk a kép betöltéséhez
     static{
         try{
             oktatoImage = ImageIO.read(new File(OKTATO_IMAGE_PATH));
@@ -21,31 +33,69 @@ public class OktatoView extends KarakterView{
         }
     }
 
+
+    /**
+     * Alapértelmezett konstruktor, amely üres modellt és koordinátákat állít be
+     */
     public OktatoView() {
         this.model = null;
         this.cd = new Coordinates(0, 0);
     }
 
+    /**
+     * Konstruktor, amely beállítja a modellt és a koordinátákat
+     *
+     * @param model Az Oktato modell
+     * @param cd A koordináták, ahol az oktató megjelenik
+     */
     public OktatoView(Oktato model, Coordinates cd){
         this.model = model;
         this.cd = cd;
     }
 
+    /**
+     * Getter a koordinátákhoz
+     *
+     * @return A koordináták, ahol az oktató megjelenik
+     */
     public Coordinates getCd() {return cd;}
 
+    /**
+     * Setter a koordinátákhoz
+     *
+     * @param cd A koordináták, ahol az oktató megjelenik
+     */
     public void setCd(Coordinates cd) {this.cd = cd;}
 
+    /**
+     * Védett getter a modellhez
+     *
+     * @return Az Oktato modell
+     */
     protected Oktato getModel(){return model;}
+
+    /**
+     * Védett setter a modellhez
+     *
+     * @param model Az Oktato modell
+     */
     protected void setModel(Oktato model){this.model=model;}
 
+
+    /**
+     * A Graphics objektum segítségével megrajzoljuk az oktatót
+     *
+     * @param g A Graphics objektum, amellyel rajzolunk
+     */
     @Override
     public void draw(Graphics g) {
         if (oktatoImage != null){
+            // Ha a kép sikeresen betöltődött, megrajzoljuk
             g.drawImage(oktatoImage, cd.getX(), cd.getY(),115 , 280, null);
         }
         else{
-            // Draw a placeholder rectangle if the image failed to load
+            // Ha a kép betöltése nem sikerült, egy helykitöltő téglalapot rajzolunk
             g.setColor(Color.RED);
-            g.fillRect(cd.getX(), cd.getY(), 100, 100); // Adjust the size as needed
+            g.fillRect(cd.getX(), cd.getY(), 100, 100); // Igazítsa a méretet szükség szerint
         }}
 }
